a32e70d970d22dc0455dbf7be88c16ae2161195a
jadestoner <jadestoner@jadestonerdeMacBook-Pro.local>
2025-07-15 22:44:18 +0800

    connection 复用

--- utils/rpc.js
+++ utils/rpc.js
@@ -2,6 +2,8 @@ import { RPC_LIST } from "./config.js";
 import { Connection, PublicKey } from "@solana/web3.js";
 
 let currentRpcIndex = 0;
+let connection = null;
+
 
 export function getCurrentRpc() {
     return RPC_LIST[currentRpcIndex];
@@ -10,9 +12,14 @@ export function getCurrentRpc() {
 export function switchRpc() {
     currentRpcIndex = (currentRpcIndex + 1) % RPC_LIST.length;
     console.log("[RPC切换] 已切换到下一个RPC节点:", getCurrentRpc());
+    connection = null;
     return getCurrentRpc();
 }
 
 export function getConnection() {
-    return new Connection(getCurrentRpc(), "confirmed");
+    if(connection != null){
+        return connection;
+    }
+    connection  = new Connection(getCurrentRpc(), "confirmed");
+    return connection;
 }
